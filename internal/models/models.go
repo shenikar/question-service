5a49feb2dc4efd146eef7cc6c4ddbaa0aac97323
shenikar <shenikar@student.21-school.ru>
2025-11-11 13:44:10 +0300

    added validator

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,7 +9,7 @@ import (
 // Question представляет модель вопроса
 type Question struct {
 	ID        uint      `gorm:"primaryKey"`
-	Text      string    `gorm:"not null"`
+	Text      string    `gorm:"not null" validate:"required,min=3,max=500"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	Answers   []Answer  `gorm:"foreignKey:QuestionID;constraint:OnDelete:CASCADE;"`
 }
@@ -19,6 +19,6 @@ type Answer struct {
 	ID         uint      `gorm:"primaryKey"`
 	QuestionID uint      `gorm:"not null"`
 	UserID     uuid.UUID `gorm:"not null"`
-	Text       string    `gorm:"not null"`
+	Text       string    `gorm:"not null" validate:"required,min=3,max=500"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 }
