7af6ee66eb0288264694caef4dd283a03205e9cd
shenikar <shenikar@student.21-school.ru>
2025-11-11 11:58:20 +0300

    refactoring cfg

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -10,43 +9,23 @@ import (
 
 // Config хранит все конфигурации приложения.
 type Config struct {
-	Database DatabaseConfig
-}
-
-type DatabaseConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Name     string
-	SSLMode  string
+	DatabaseURL string
 }
 
 // Load считывает конфигурацию из .env файла или переменных окружения.
 func Load() (*Config, error) {
 	if err := godotenv.Load(); err != nil {
-		log.Println("Error loading .env file")
+		log.Println("Info: .env file not found, loading from environment variables")
 	}
+
 	config := &Config{
-		Database: DatabaseConfig{
-			Host:     os.Getenv("DB_HOST"),
-			Port:     os.Getenv("DB_PORT"),
-			User:     os.Getenv("DB_USER"),
-			Password: os.Getenv("DB_PASSWORD"),
-			Name:     os.Getenv("DB_NAME"),
-			SSLMode:  os.Getenv("DB_SSLMODE"),
-		},
+		DatabaseURL: os.Getenv("DATABASE_URL"),
 	}
+
 	return config, nil
 }
 
-// GetDatabaseUrl формирует строку подключения к базе данных
+// GetDatabaseURL возвращает строку подключения к базе данных.
 func (c *Config) GetDatabaseURL() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
-		c.Database.User,
-		c.Database.Password,
-		c.Database.Host,
-		c.Database.Port,
-		c.Database.Name,
-		c.Database.SSLMode)
+	return c.DatabaseURL
 }
